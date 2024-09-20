a52e117e689b99fc51bad2535f447cc72315ac92
tanveerkhan7773 <65626721+tanveerkhan7773@users.noreply.github.com>
2024-09-20 07:30:41 +0000

    my edits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,20 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-st.title('Hello')
+#import matplotlib.pyplot as plt
 
-st.write('*Data Science Course 2024')
+st.title('Hello world')
 
-df=pd.read_csv('data/Bastar Craton.csv')
+st.write('*Data Science Course 2024*')
+file_path='data/BastarCraton.csv'
+df = pd.read_csv('data/Bastar Craton.csv')
+cat_names = df.columns.tolist()[27:]
+st.title('Hello world')
+el1 = st.selectbox('select element', cat_names = 
+  )
+st.write (el1)
+with col1;
+st.dataframe(df)
+st.write(el1)
 
-st.write(df)
+with col2:
+    el2
