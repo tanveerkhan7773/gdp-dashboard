7eee08acb8f1c1d24ca86aa854607990fe90fccf
tanveerkhan7773 <65626721+tanveerkhan7773@users.noreply.github.com>
2024-09-20 07:03:05 +0000

    edit packages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,10 @@
 import streamlit as st
-st.title('Hello Freinds')
+import pandas as pd
+import matplotlib.pyplot as plt
+st.title('Hello')
+
+st.write('*Data Science Course 2024')
+
+df=pd.read_csv('data/Bastar Craton.csv')
+
+st.write(df)
